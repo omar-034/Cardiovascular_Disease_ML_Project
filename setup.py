aee17942e25baee865d010248e65a8d6432bf9ea
Omar <od14034@gmail.com>
2026-08-26 04:07:16 +0000

    refractor: Fix some bugs relating to encoding file requirement

--- setup.py
+++ setup.py
@@ -2,25 +2,28 @@ from setuptools import setup, find_packages
 from typing import List
 
 HYPHEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->List[str]:
+
+def get_requirements(file_path: str) -> List[str]:
     '''
     This function will return a list of requirements from the given file path.
     '''
-    requirements=[]
-    with open(file_path) as file_obj:
+    requirements = []
+    with open(file_path, encoding='utf-8-sig') as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-        
+        requirements = [req.strip() for req in requirements]
+
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-    
+
     return requirements
 
-
 setup(
-    name='Cardio Disease Prediction',
+    name='cardio-disease-prediction',
     version='0.1',
     author='Omar Diop',
     author_email='od14034@gmail.com',
+    description='Prédiction du risque de maladie cardiovasculaire à partir de données cliniques',
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt'))
+    install_requires=get_requirements('requirements.txt'),
+    python_requires='>=3.10',
+)
